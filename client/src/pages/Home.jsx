1af190adc6763a8fa73e7e69fab50170e580df06
tanveerlari <shamsherlari@gmail.com>
2026-04-28 18:28:58 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
       <Hero />
+      <AiTools />
     
     </>
   )
